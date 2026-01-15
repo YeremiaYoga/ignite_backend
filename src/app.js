4a6c69dd9047cd75fa9c4b520a1a3d6f34361901
Yeremia Yoga <yogayeremia@gmail.com>
2026-01-15 14:32:54 +0700

    update page menu

--- src/app.js
+++ src/app.js
@@ -60,6 +60,7 @@ import adminHomebrewRoutes from "./routes/adminHomebrewSourceRoutes.js";
 import igniteHomebrewRoutes from "./routes/igniteHomebrewRoutes.js";
 import igniteJournalRoutes from "./routes/igniteJournalRoutes.js";
 import igniteCharacterUsageRoutes from "./routes/igniteCharacterUsageRoutes.js";
+import igniteFeatureRoutes from "./routes/igniteFeatureRoutes.js";
 
 import path from "path";
 const app = express();
@@ -160,5 +161,6 @@ app.use("/ignite/calendars", igniteCalendarRoutes);
 app.use("/ignite/calendar-event", igniteCalendarEventRoutes);
 app.use("/ignite/journals", igniteJournalRoutes);
 app.use("/ignite/character-limit", igniteCharacterUsageRoutes);
+app.use("/ignite/features", igniteFeatureRoutes);
 app.use("/foundry/features", foundryFeatureRoutes);
 export default app;
